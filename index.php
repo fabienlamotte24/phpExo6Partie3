6c23e646f4d04f3fd1d94c4041784cd6a814e5f5
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-26 14:27:46 +0200

    exo6p3 php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 6</title>
+  </head>
+  <body>
+<p>
+<?php
+  $pas = 20;
+  while($pas > 0){
+    echo $pas . ' pas, c\'est presque bon <br />';
+    $pas --;
+  }
+?>
+</p>
+  </body>
+</html>
